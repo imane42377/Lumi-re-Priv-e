da61e06d5a19063b7d9b4d71e1e79c0cb8dd96d2
imane42377 <imanemeliane18@gmail.com>
2026-04-29 12:28:23 +0100

    modify main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { LanguageProvider } from './context/language_context.tsx'
+import { LanguageProvider } from './context/language_context'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './context/theme-context.tsx'
+import { ThemeProvider } from './context/theme-context'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
